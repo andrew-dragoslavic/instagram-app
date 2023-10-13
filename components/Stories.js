c1cf9cb99b4e69d4cf9ec7b3ff3c63d3b0a92f66
Andrew <andrew.dragoslavic@gmail.com>
2023-10-13 13:59:29 -0400

    First commit

---
+++ components/Stories.js
@@ -0,0 +1,31 @@
+import { faker } from '@faker-js/faker';
+import { useEffect, useState } from "react";
+
+function Stories() {
+
+    const [suggestions, setSuggestions] = useState([])
+
+    useEffect(() => {
+        const suggestions = [...Array(20)].map((_, i) => ({
+            ...faker.helpers.contextualCard(),
+            id: i,
+        }));
+        setSuggestions(suggestions);
+    }, [])
+
+    return (
+        <div>
+            {/* Story */}
+            {/* Story */}
+            {/* Story */}
+            {/* Story */}
+            {/* Story */}
+            {/* Story */}
+            {/* Story */}
+        </div>
+    );
+}
+
+export default Stories;
+
+
--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    images: {
+        domains: ["links.papareact.com", "https://media.licdn.com/dms/image/C5603AQEtQDtjXFZCBA/profile-displayphoto-shrink_800_800/0/1657087278014?e=2147483647&v=beta&t=CijXVNqWKMp-HmPZ_Nj42Xh1Ji9PZpW0RTNKxkCs8sI"],
+    },
+};
